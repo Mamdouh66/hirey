4e93dcbdb375dc7347ff846babaa2f28a679beff
Mamdouh Khalaf <2210002016@iau.edu.sa>
2024-04-19 20:44:51 +0300

    🧹 ADD: dummy route

--- hirey/api/server.py
+++ hirey/api/server.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
+from hirey.api.routes import router as api_router
+
 
 def get_application():
-    app = FastAPI(title="Hirey", version="0.1.0") # TODO: Add through config
+    app = FastAPI(title="Hirey", version="0.1.0")  # TODO: Add through config
 
     app.add_middleware(
         CORSMiddleware,
@@ -12,6 +14,8 @@ def get_application():
         allow_methods=["*"],
         allow_headers=["*"],
     )
+
+    app.include_router(api_router, prefix="/api")
 
     return app
 
